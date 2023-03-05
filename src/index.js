3bdc95a752bc413030486de30092a1a310706fa3
Damiano Casula <d.casula@factor-eleven.de>
2023-03-05 17:55:54 +0100

    new: temp test of searchImages function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import searchImages from './api';
+
+searchImages().then((response) => {
+  console.log(response);
+});
 
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
